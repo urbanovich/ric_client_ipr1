a1a8368fca13060016d646e3338621f4256e22c1
Urbanovich Dzmitry <urbanovich.mslo@gmail.com>
2019-12-09 23:26:12 +0300

    added menu

--- src/ric_client_ipr1/Ric_client_ipr1.java
+++ src/ric_client_ipr1/Ric_client_ipr1.java
@@ -7,6 +7,8 @@ package ric_client_ipr1;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+import org.json.simple.*;
 
 /**
  *
@@ -27,24 +29,64 @@ public class Ric_client_ipr1 {
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
  
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String text;
+            Scanner scn = new Scanner(System.in);
+            String command;
  
             do {
-                System.out.println("Enter text: ");
+                System.out.println("If you want add new an element set 1.");
+                System.out.println("If you want to find an element set 2.");
+                System.out.println("If you want to delete an element set 3.");
+                System.out.println("If you want to exit set 4.");
                 
-                text = in.readLine();
+                command = scn.nextLine();
  
-                writer.println(text);
+                JSONObject row = new JSONObject();
+                row.put("command", command);
+                
+                switch (Integer.parseInt(command)) {
+                    case 1:
+                        
+                        System.out.println("Set id:");
+                        String id = scn.nextLine();
+                        row.put("id", id);
+
+                        System.out.println("Set title:");
+                        String title = scn.nextLine();
+                        row.put("title", title);
+
+                        System.out.println("Set content:");
+                        String content = scn.nextLine();
+                        row.put("content", content);
+                        
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                }
+                
+                writer.println(row.toString());
  
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String result = new String();                
+                String line = new String();                
+                while ((line = reader.readLine()) != null) {
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    result += line + "\n";
+                }
  
-                String time = reader.readLine();
+                System.out.println(result);
  
-                System.out.println(time);
- 
-            } while (!text.equals("bye"));
+            } while (!command.equals("4"));
  
             socket.close();
  
